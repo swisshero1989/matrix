4e8f5385a4ef59c1f745f66f19f13686dc6fbf1d
swisshero1989 <159089709+swisshero1989@users.noreply.github.com>
2024-02-08 09:08:55 +0100

    Replace class with fp approach

--- index.js
+++ index.js
@@ -128,63 +128,61 @@ function rand(start, end) {
   return start + Math.floor(Math.random() * (end - start));
 }
 
-class MatrixRain {
-  constructor(opts) {
-    this.transpose = opts.direction === 'h';
-    this.color = opts.color;
-    this.charRange = opts.charRange;
-    this.maxSpeed = 20;
-    this.colDroplets = [];
-    this.numCols = 0;
-    this.numRows = 0;
+function MatrixRain(opts) {
+  const transpose = opts.direction === 'h';
+  const color = opts.color;
+  const charRange = opts.charRange;
+  const maxSpeed = 20;
+  const colDroplets = [];
+  const numCols = 0;
+  const numRows = 0;
 
-    // handle reading from file
-    if (opts.filePath) {
-      if (!fs.existsSync(opts.filePath)) {
-        throw new Error("${opts.filePath} doesn't exist");
-      }
-      this.fileChars = fs.readFileSync(opts.filePath, 'utf-8').trim().split('');
-      this.filePos = 0;
-      this.charRange = 'file';
+  // handle reading from file
+  if (opts.filePath) {
+    if (!fs.existsSync(opts.filePath)) {
+      throw new Error("${opts.filePath} doesn't exist");
     }
-
-    // handle ascii art mask
-    if (opts.maskPath) {
-      this.maskConf = {
-        filepath: opts.maskPath,
-        alphabet: 'bits',
-        width: this.numCols,
-        height: this.numRows * opts.fontRatio,
-      };
-      this.maskInverted = opts.invertMask;
-      this.mask = undefined;
-      this.fontRatio = opts.fontRatio;
-      this.maskWidth = 0;
-      this.maskHeight = 0;
-      this.maskOffsetRow = opts.offsetRow;
-      this.maskOffsetCol = opts.offsetCol;
-      this.maskBlankChar = ' ';
-    }
-
-    if (opts.printMask) {
-      if (!opts.maskPath) {
-        console.log('no mask file provided.');
-        stop();
-      }
-
-      this.computeMask().then((mask) => {
-        for (let r in [(0).this.maskOffsetRow]) {
-          console.log('');
-        }
-        mask.forEach((row, i) => {
-          console.log(' '.repeat(this.maskOffsetCol), row);
-        });
-        stop();
-      });
-    }
+    fileChars = fs.readFileSync(opts.filePath, 'utf-8').trim().split('');
+    filePos = 0;
+    charRange = 'file';
   }
 
-  generateChars(len, charRange) {
+  // handle ascii art mask
+  if (opts.maskPath) {
+    maskConf = {
+      filepath: opts.maskPath,
+      alphabet: 'bits',
+      width: numCols,
+      height: numRows * opts.fontRatio,
+    };
+    maskInverted = opts.invertMask;
+    mask = undefined;
+    fontRatio = opts.fontRatio;
+    maskWidth = 0;
+    maskHeight = 0;
+    maskOffsetRow = opts.offsetRow;
+    maskOffsetCol = opts.offsetCol;
+    maskBlankChar = ' ';
+  }
+
+  if (opts.printMask) {
+    if (!opts.maskPath) {
+      console.log('no mask file provided.');
+      stop();
+    }
+
+    computeMask().then((mask) => {
+      for (let r in [(0).maskOffsetRow]) {
+        console.log('');
+      }
+      mask.forEach((row, i) => {
+        console.log(' '.repeat(maskOffsetCol), row);
+      });
+      stop();
+    });
+  }
+
+  function generateChars(len, charRange) {
     // by default charRange == ascii
     let chars = new Array(len);
 
@@ -216,9 +214,9 @@ class MatrixRain {
       }
     } else if (charRange === 'lil-guys') {
       // Force horizontal direction
-      if (!this.transpose) {
-        this.transpose = true;
-        this.color = 'white';
+      if (!transpose) {
+        transpose = true;
+        color = 'white';
         start();
       }
 
@@ -226,72 +224,69 @@ class MatrixRain {
         chars[i] = '  ~~o ';
       }
     } else if (charRange === 'file') {
-      for (let i = 0; i < len; i++, this.filePos++) {
-        this.filePos = this.filePos < this.fileChars.length ? this.filePos : 0;
-        chars[i] = this.fileChars[this.filePos];
+      for (let i = 0; i < len; i++, filePos++) {
+        filePos = filePos < fileChars.length ? filePos : 0;
+        chars[i] = fileChars[filePos];
       }
     }
 
     return chars;
   }
 
-  makeDroplet(col) {
+  function makeDroplet(col) {
     return {
       col,
       alive: 0,
-      curRow: rand(0, this.numRows),
-      height: rand(this.numRows / 2, this.numRows),
-      speed: rand(1, this.maxSpeed),
-      chars: this.generateChars(this.numRows, this.charRange),
+      curRow: rand(0, numRows),
+      height: rand(numRows / 2, numRows),
+      speed: rand(1, maxSpeed),
+      chars: generateChars(numRows, charRange),
     };
   }
 
-  resizeDroplets() {
-    [this.numCols, this.numRows] = process.stdout.getWindowSize();
+  function resizeDroplets() {
+    [numCols, numRows] = process.stdout.getWindowSize();
 
-    if (this.maskConf) {
-      this.maskConf.width = this.numCols;
-      this.maskConf.height = this.numRows * this.fontRatio;
-      this.computeMask().then((mask) => (this.mask = mask));
+    if (maskConf) {
+      maskConf.width = numCols;
+      maskConf.height = numRows * fontRatio;
+      computeMask().then((mask) => (mask = mask));
     }
 
     // transpose for direction
-    if (this.transpose) {
-      [this.numCols, this.numRows] = [this.numRows, this.numCols];
+    if (transpose) {
+      [numCols, numRows] = [numRows, numCols];
     }
 
     // Create droplets per column
     // add/remove droplets to match column size
-    if (this.numCols > this.colDroplets.length) {
-      for (let col = this.colDroplets.length; col < this.numCols; ++col) {
+    if (numCols > colDroplets.length) {
+      for (let col = colDroplets.length; col < numCols; ++col) {
         // make two droplets per row that start in random positions
-        this.colDroplets.push([this.makeDroplet(col), this.makeDroplet(col)]);
+        colDroplets.push([makeDroplet(col), makeDroplet(col)]);
       }
     } else {
-      this.colDroplets.splice(
-        this.numCols,
-        this.colDroplets.length - this.numCols
-      );
+      colDroplets.splice(numCols, colDroplets.length - numCols);
     }
   }
 
-  writeAt(row, col, str, color) {
+  function writeAt(row, col, str, color) {
     // Only output if in viewport
-    if (row >= 0 && row < this.numRows && col >= 0 && col < this.numCols) {
-      if (this.transpose) {
+    if (row >= 0 && row < numRows && col >= 0 && col < numCols) {
+      if (transpose) {
         [col, row] = [row, col];
       }
       const pos = ansi.cursorPos(row, col);
-      if (this.mask) {
-        const maskRow = row - this.maskOffsetRow;
-        const maskCol = col - this.maskOffsetCol;
+      if (mask) {
+        const maskRow = row - maskOffsetRow;
+        const maskCol = col - maskOffsetCol;
         if (
           maskRow >= 0 &&
           maskCol >= 0 &&
-          maskRow < this.maskHeight &&
-          maskCol < this.maskWidth &&
-          this.mask[maskRow] &&
-          this.mask[maskRow][maskCol] === this.maskBlankChar
+          maskRow < maskHeight &&
+          maskCol < maskWidth &&
+          mask[maskRow] &&
+          mask[maskRow][maskCol] === maskBlankChar
         ) {
           str = ' ';
         }
@@ -300,37 +295,25 @@ class MatrixRain {
     }
   }
 
-  renderFrame() {
+  function renderFrame() {
     const ansiColor =
-      ansi.colors[
-        'fg${this.color.charAt(0).toUpperCase()}${this.color.substr(1)}'
-      ]();
+      ansi.colors['fg${color.charAt(0).toUpperCase()}${color.substr(1)}']();
 
-    for (const droplets of this.colDroplets) {
+    for (const droplets of colDroplets) {
       for (const droplet of droplets) {
         const { curRow, col: curCol, height } = droplet;
         droplet.alive++;
 
         if (droplet.alive % droplet.speed === 0) {
-          this.writeAt(
-            curRow - 1,
-            curCol,
-            droplet.chars[curRow - 1],
-            ansiColor
-          );
-          this.writeAt(
-            curRow,
-            curCol,
-            droplet.chars[curRow],
-            ansi.colors.fgWhite()
-          );
-          this.writeAt(curRow - height, curCol, ' ');
+          writeAt(curRow - 1, curCol, droplet.chars[curRow - 1], ansiColor);
+          writeAt(curRow, curCol, droplet.chars[curRow], ansi.colors.fgWhite());
+          writeAt(curRow - height, curCol, ' ');
           droplet.curRow++;
         }
 
-        if (curRow - height > this.numRows) {
+        if (curRow - height > numRows) {
           // reset droplet
-          Object.assign(droplet, this.makeDroplet(droplet.col), { curRow: 0 });
+          Object.assign(droplet, makeDroplet(droplet.col), { curRow: 0 });
         }
       }
     }
@@ -338,9 +321,9 @@ class MatrixRain {
     flush();
   }
 
-  computeMask() {
+  function computeMask() {
     return new Promise((resolve, reject) => {
-      new art.Image(Object.assign({}, this.maskConf)).write((err, render) => {
+      new art.Image(Object.assign({}, maskConf)).write((err, render) => {
         if (err) {
           console.error(err);
           stop();
@@ -348,13 +331,22 @@ class MatrixRain {
         }
         let mask = strip(render).split('\n');
         mask = mask.slice(0, mask.length - 1);
-        this.maskWidth = mask[0].length;
-        this.maskHeight = mask.length;
-        this.maskBlankChar = this.maskInverted ? '#' : ' ';
+        maskWidth = mask[0].length;
+        maskHeight = mask.length;
+        maskBlankChar = maskInverted ? '#' : ' ';
         resolve(mask);
       });
     });
   }
+
+  return {
+    generatChars: generateChars,
+    makeDroplet: makeDroplet,
+    resizeDroplets: resizeDroplets,
+    writeAt: writeAt,
+    renderFrame: renderFrame,
+    computeMask: computeMask,
+  };
 }
 
 //// main ////
